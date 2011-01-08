4a079416f26540fc2e54e688de3f5a7b5fd78eb5
Marcus Ramberg <mramberg@cpan.org>
2011-01-08 11:03:42 +0000

    Import of MRAMBERG/Mojolicious-Plugin-OAuth2-0.01 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-OAuth2
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MRAMBERG/Mojolicious-Plugin-OAuth2-0.01.tar.gz
    gitpan-cpan-author:       MRAMBERG
    gitpan-cpan-maturity:     released

---
+++ lib/Mojolicious/Plugin/OAuth2.pm
@@ -0,0 +1,182 @@
+package Mojolicious::Plugin::OAuth2;
+
+use base qw/Mojolicious::Plugin/;
+use Carp qw/croak/;
+use strict; 
+
+our $VERSION='0.01';
+
+__PACKAGE__->attr(providers=>sub {
+    return {
+        facebook => {
+            authorize_url => "https://graph.facebook.com/oauth/authorize",
+            token_url => "https://graph.facebook.com/oauth/access_token",
+        },
+        dailymotion => {
+            authorize_url => "https:/api.dailymotion.com/oauth/authorize",
+            token_url => "https:/api.dailymotion.com/oauth/token"
+        },
+        gowalla => {
+            authorize_url => "https://gowalla.com/api/oauth/new",
+            token_url     => "https://api.gowalla.com/api/oauth/token",
+        },
+    };
+});
+
+sub register {
+    my ($self,$app,$config)=@_;
+    my $providers=$self->providers;
+    foreach my $provider (keys %$config) {
+        if(exists $providers->{$provider}) {
+            foreach my $key (keys %{$config->{$provider}}) {
+                $providers->{$provider}->{$key}=$config->{$provider}->{$key};
+            }
+        } else {
+            $providers->{$provider}=$config->{$provider};
+        }
+    }
+    $self->providers($providers);
+    
+    $app->renderer->add_helper(
+        get_token => sub {
+            my ($c,$provider_id,%args)= @_;
+            croak "Unknown provider $provider_id" 
+                unless (my $provider=$self->providers->{$provider_id});
+            if($c->param('code')) {
+                my $fb_url=Mojo::URL->new($provider->{token_url});
+                $fb_url->query->append(
+                    client_secret=> $provider->{secret},
+                    client_id=> $provider->{key},
+                    code => $c->param('code'),
+                    redirect_uri=>$c->url_for->to_abs->to_string,
+                );
+                my $client = $args{async} ? $c->client->async : $c->client;
+                $client->get($fb_url->to_abs => sub {
+                    my ($client,$tx)=@_;
+                    if (my $res=$tx->success) {
+                        my $qp=Mojo::Parameters->new($res->body);
+                        &{$args{callback}}($qp->param('access_token') );
+                    }
+                    else {
+                        my ($err)=$tx->error;
+                        &{$args{error_handler}}($tx) if(exists $args{error_handler});
+                    }
+                })->start;
+                $c->render_later if $args{async};
+            } else {
+                my $fb_url=Mojo::URL->new($provider->{authorize_url});
+                $fb_url->query->append(
+                    client_id=> $provider->{key},
+                    redirect_uri=>$c->url_for->to_abs->to_string,
+                );
+                $fb_url->query->append(scope => $args{scope}) 
+                    if exists $args{scope};
+                $c->redirect_to($fb_url);
+             }
+    });
+}
+
+1;
+
+=head1 NAME
+
+Mojolicious::Plugin::OAuth2 - Auth against OAUth2 APIs
+
+=head1 SYNOPSIS 
+
+   plugin 'oauth2',
+       facebook => {
+          key => 'foo',
+          secret => 'bar' 
+       };
+   
+   get '/auth' => sub {
+      my $self=shift;
+      $self->get_token('facebook',callback=>sub {
+         ...
+      });
+   };
+
+=head1 DESCRIPTION
+
+This Mojolicious plugin allows you to easily authenticate against a OAuth2 
+provider. It includes configurations for a few popular providers, but you 
+can add your own easily as well.
+
+Note that OAuth2 requires https, so you need to have the optional Mojolicious 
+dependency required to support it. Call
+
+   $ mojo version
+
+to check if it is installed. 
+
+=head1 HELPERS
+
+=head2 get_token <$provider>, <%args>
+
+Will redirect to the provider to allow for authorization, then fetch the 
+token. The token gets provided as a parmeter to the callback function. 
+Usually you want to store the token in a session or similar to use for 
+API requests. Supported arguments:
+
+=over 4
+
+=item callback
+
+Callback method to handle the provided token. Gets the token as it's only argument
+
+=item error_callback
+
+Callback method to handle any error. Gets the failed transaction as it's only argument.
+
+=item scope
+
+Scope to ask for credentials to. Should be a space separated list.
+
+=item async
+
+Use async request handling to fetch token.
+
+=back
+
+=head1 CONFIGURATION
+
+=head2 providers 
+
+Takes a hashref of providers, each one with a hashref of options. For instance:
+
+    plugin 'oauth2', {
+       iusethis => {
+          authorize_url => 'iut.com/auth',
+          token_url => 'iut.com/token',
+          key => 'foo',
+          secret => 'bar',
+    }};
+
+The plugin includes configurations a few providers, to use those, just set the key and secret. The currently supported providers are:
+
+=over 4
+
+=item facebook
+
+OAuth for facebook's graph API, L<http://graph.facebook.com/>.
+
+=item dailymotion
+
+Authentication for Dailymotion video site.
+
+=item gowalla
+
+Gowalla.com authentication.
+
+=back
+
+=head2 AUTHOR
+
+Marcus Ramberg L<mailto:mramberg@cpan.org>
+
+=head2 LICENSE
+
+This software is licensed under the same terms as Perl itself.
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,62 @@
+use Mojolicious::Lite;
+use Test::Mojo;
+use Test::More;
+
+my $t=Test::Mojo->new;
+my $port = $t->client->test_server;
+
+plugin 'o_auth2', test => {
+    authorize_url => Mojo::URL->new("http://localhost:$port/fake_auth"),
+    token_url => Mojo::URL->new("http://localhost:$port/fake_token"),
+    key    => 'fake_key',
+    secret => 'fake_secret',
+};
+
+get '/oauth' => sub { 
+    my $self=shift;
+    $self->get_token('test', callback => sub {
+        my $token=shift;
+        $self->render(text=>'Token '.$token);
+    },
+    error_handler => sub { status=>500,$self->render(text=>'oauth failed to get'.shift->req->uri)},
+    async => 1,
+    scope => 'fakescope');
+} => 'foo';
+
+get 'fake_auth' => sub {
+    my $self=shift;
+    if ($self->param('client_id') && $self->param('redirect_uri') &&$self->param('scope')) {
+        my $return=Mojo::URL->new($self->param('redirect_uri'));
+        $return->query->append(code=>'fake_code');
+        $self->redirect_to($return);
+    }
+    else {
+        $self->render(status=>404,text=>'REJECTED');
+    }
+};
+
+get 'fake_token' => sub {
+    my $self=shift;
+    if($self->param('client_secret') && 
+       $self->param('redirect_uri') && 
+       $self->param('code')) {
+           my $qp=Mojo::Parameters->new(access_token=>'fake_token',lifetime=>3600);
+        $self->render(text=>$qp->to_string);
+    }
+    else {
+        $self->render(status=>404,text=>"FAIL OVERFLOW");
+    }
+};
+
+
+$t->get_ok('/oauth')->status_is(302); # ->content_like(qr/bar/);
+my $location=Mojo::URL->new($t->tx->res->headers->location);
+my $callback_url=Mojo::URL->new($location->query->param('redirect_uri'));
+is($location->query->param('client_id'),'fake_key');
+$t->get_ok($location)->status_is(302);
+my $res=Mojo::URL->new($t->tx->res->headers->location);
+is($res->path,$callback_url->path,'Returns to the right place');
+is($res->query->param('code'),'fake_code','Includes fake code');
+$t->get_ok($res)->status_is(200)->content_like(qr/fake_token/);
+
+done_testing;
